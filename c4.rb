a782af4031b2de584b2d277d65d07ee80a9a3ce6
Calvin <Calvin.lacc@gmail.com>
2016-06-21 13:12:47 -0600

    removed pusdo

--- c4.rb
+++ c4.rb
@@ -1,75 +1,14 @@
-# Psueodocode
-# class Connect4
-	#initialize
-		#set up players
-		#set up board
-		#assign starting player
-
-
-	#play
-		#loop until break
-			#render board
-			#current player (p1)- ask for column 
-			#check win/tie
-			#change player
-
-	#valid move
-
-	#game_over
-		#check win || tie
-
-	#check win
-
-	#check tie
-
-	#switch player
-
-#Board
-	#initialize 
-		#7 column, 6 row board
-
-	#render 
-	#display the empty or used board
-
-	# add piece
-
-	# get_coordinates from player
-
-	#valid coordinates input type?
-
-	#valid move avaliable?
-
-	#define win
-		#LOT OF CODE
-
-#Player
-	#initialize 
-		#ask for player name
-		#display player color
-
-	#get_coordinates
-			#loop until valid input
-			#call ask_for_coordinates
-			#check for format
-			#check if valid input type (call board method)
-			# check if valid_move_avaliable (call board method)
-			#connect to add piece
-
-	#ask_for coordinates
-
-
-	#check for format 
-
 require 'board.rb' 
 require 'player.rb'
+require 'pry'
 
 
 class Connect4	
 	def initialize
 
 		@board= Board.new
-		@player1= Player.new("aash",:X,@board)
-		@player2= Player.new("calvin",:O,board)
+		@player1= Player.new("aash","X",@board)
+		@player2= Player.new("calvin","O",@board)
 
 		@currentplayer=@player1
 	end
@@ -83,23 +22,24 @@ class Connect4
 	end
 
 	def game_over?
-		if check_tie || check_win
+		# if check_tie || check_win
+		if check_win
 			return true
 		else 
 			return false
 		end
 	end
 
-	def check_tie?
-		if @board.full 
-			true
-		else 
-			false
-		end
-	end
+	# def check_tie?
+	# 	if @board.full 
+	# 		true
+	# 	else 
+	# 		false
+	# 	end
+	# end
 
 	def check_win?
-		if @board.winning_combination
+		if @board.winning_combination("X")
 			true
 		else 
 			false
