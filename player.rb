f9aa6409239a841ab5e28e6d9fe4f3fe9fbb234e
Calvin <Calvin.lacc@gmail.com>
2016-06-23 11:15:25 -0600

    Fixed rows and columns

--- player.rb
+++ player.rb
@@ -1,39 +1,32 @@
-class Player 
+class Player
    def initialize(name,symbol,board)
        @name=name
        @symbol=symbol
        @board=board
    end
    attr_accessor :name, :symbol
-
    def get_coordinates
        while true
            column=ask_for_coordinates
            if check_for_format(column.to_i)
-           		puts "check"
                if @board.add_piece(column,@symbol)
-               	puts "add "
                    break
                end
            end
        end
    end
-
    def everything_valid
    end
-
    def ask_for_coordinates
-       puts "Please enter a valid column number: "
+       puts "#{name}, where would you like to place a game piece? "
        column_number=gets.chomp
    end
-
    def check_for_format(column)
        if column>=0 && column<=6
-           puts "Good format"
            return true
        else
            puts "Wrong Format"
-           return false 
+           return false
        end
    end
 end
